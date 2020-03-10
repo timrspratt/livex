a7349e94cc26ff084911c839bfb2cb594d10c0cf
Tim Spratt <timspratt@hotmail.co.uk>
2020-03-10 16:00:49 +0000

    Update ServiceProvider.php

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -3,7 +3,9 @@
 namespace Sypo\Livex;
 
 use Aero\Admin\AdminModule;
+use Aero\Common\Facades\Settings;
 use Aero\Common\Providers\ModuleServiceProvider;
+use Aero\Common\Settings\SettingGroup;
 
 class ServiceProvider extends ModuleServiceProvider
 {
@@ -15,5 +17,18 @@ class ServiceProvider extends ModuleServiceProvider
             #->permissions('Livex.custom') //optional
             ->routes(dirname(__DIR__).'/../routes/admin.php')
             ->route('admin.modules.livex');
+    }
+    
+    public function boot(): void
+    {
+        parent::boot();
+        
+        Settings::group('Livex', function (SettingGroup $group) {
+            $group->boolean('enabled')->default(true);
+            $group->float('still_wine_rate')->default(26.78);
+            $group->float('sparkling_wine_rate')->default(34.30);
+            $group->float('fortified_wine_rate')->default(35.70);
+            $group->integer('litre_calc')->default(9);
+        });
     }
 }
